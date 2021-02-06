b61cb3ab55ec355014ae4e391ba60e8c3fca42e4
MCpracticesforskills <72710813+MCpracticesforskills@users.noreply.github.com>
2021-02-05 20:09:22 -0800

    added alerts and prompts to js file
    full name alerts

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-alert("Hello Gorgeous")
+ 
+let firstName = prompt('What is your first name');
+let lastName = prompt('What is your last name');
+let fullName = firstName + ' ' + lastName;
+alert(fullName); 
+
+
 
